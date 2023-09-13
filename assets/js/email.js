b761722aa7d9d5576fbec8533b2e2e996f83a70a
Petr Doležal <sirelkir@gmail.com>
2023-09-13 14:48:15 +0100

    Added email and ecmwf section

---
+++ assets/js/email.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var emailLink = document.getElementById('email-link');
+    emailLink.href = "mailto:" + "pd423" + "@" + "cst.cam.ac.uk";
+    emailLink.innerHTML = "pd423" + "@" + "cst.cam.ac.uk";
+});
