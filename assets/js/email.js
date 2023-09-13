8c45c0cc8e3405e4aa2d32b2ad51746b2054d0fa
Petr Doležal <sirelkir@gmail.com>
2023-09-13 16:33:27 +0100

    fix email and links

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var emailLink = document.getElementById('email-link');
     emailLink.href = "mailto:" + "pd423" + "@" + "cst.cam.ac.uk";
-    emailLink.innerHTML = "pd423" + "@" + "cst.cam.ac.uk";
+    var emailSpan = document.createElement('span');
+    emailSpan.classList.add('email-text');
+    emailSpan.innerText = "pd423" + "@" + "cst.cam.ac.uk";
+    emailLink.appendChild(emailSpan);
 });
